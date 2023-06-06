0201d561a52589e3eeae81ebed5594e53693aeab
Anton Mochinsky <avm199615@gmail.com>
2023-06-06 11:15:19 +0300

    first commit

---
+++ measurements/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+class Sensor(models.Model):
+    name = models.TextField()
+    description = models.CharField(max_length=50)
+
+class Measurement(models.Model):
+    temperature = models.FloatField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    sensor = models.ForeignKey(Sensor, on_delete=models.CASCADE)
